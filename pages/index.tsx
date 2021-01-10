9e1dd664678f8a338c32f9ead9454a1d09259821
dengoloborodko <den.od.1911@gmail.com>
2021-01-10 19:31:10 +0200

    :bento: content: Add title and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Head from 'next/head';
 
 import 'normalize.css';
 
-const IndexPage = () => <h1>Hello, Next</h1>;
+const IndexPage = () => (
+  <>
+    <Head>
+      <title>Memory App</title>
+      <link rel="icon" type="image/ico" href="/favicon.ico" />
+    </Head>
+    <h1>Hello, Next.js!</h1>
+  </>
+);
 
 export default IndexPage;
